14dfad695c44e8dcb438d4209cb25d7d194a4322
amulya18jan <amulyaawasth18jan@gmail.com>
2026-06-22 11:48:00 +0530

    Learning repeat method of Strings

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, nums) {
+if(nums < 0){
+    return "ERROR";
+}
+let result = "";
+for(let i =0; i< nums; i++){
+    result += str;
+}
+return result;
 };
 
 // Do not edit below this line
